a58290633b1c7617c928e6e7f353dc0ce6221c0e
BankAkira <zenbong@gmail.com>
2021-07-28 22:08:09 +0700

    Update License and Details

--- Coolroom_Monitor_AC/Coolroom_Monitor_AC.ino
+++ Coolroom_Monitor_AC/Coolroom_Monitor_AC.ino
@@ -1,4 +1,27 @@
 /*
+MIT License
+
+Copyright (c)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+
 =================
 =  Connection   =
 =================
@@ -587,3 +610,19 @@ void loop()
   }
   delay(200);
 }
+
+
+/*
+
+Cool room Monitoring System (CMS) was a research topic under Horticulture Innovation Lab Regional Center at Kasetsart University (2012-2019). 
+Under the research, there are 3 models: Online version, Offline version and SMS warning system.
+
+Objective and usage Cool room uses electricity that raises cost of the produce. 
+Learning the environmental condition surrounding cool room and behavior of using it such as the frequency of opening and closing door, duration of door opening and the cooling time of the produce will help them improve power use efficiency and safe cost. 
+Online CMS helps user to check their cool room is still running well or not anywhere in the world. If something happen such as door opened too long or temperature is too high, user will know immediately and go fix the problem and save their produce in time. 
+The system cannot controlling anything, just monitoring and warning.
+
+This material is made possible by the generous support of the American people through the United States Agency for International Development (USAID). 
+The contents are the responsibility of the Horticulture Innovation Lab and do not necessarily reflect the views of USAID or the U.S. Government.
+
+*/
